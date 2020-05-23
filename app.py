143653114536f84f8ad85ccea4f0748f7f742afb
elimywilliams <62444225+elimywilliams@users.noreply.github.com>
2020-05-22 20:29:30 -0600

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ file_name = 'https://raw.githubusercontent.com/elimywilliams/sc_covid19/master/a
 stateLags = pd.read_csv(file_name)
 stateLags['Date'] = stateLags['datetest'].astype('datetime64[ns]')
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets,suppress_callback_exceptions=True)
 
 server = app.server
 
