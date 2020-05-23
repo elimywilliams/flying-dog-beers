06eac1e6a4c972a49154aeefcb534680db81fe9d
elimywilliams <62444225+elimywilliams@users.noreply.github.com>
2020-05-22 20:31:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,122 @@ stateLags['Date'] = stateLags['datetest'].astype('datetime64[ns]')
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets,suppress_callback_exceptions=True)
 
+countryOPTS = [{'label': 'Spain', 'value': 'Spain'},
+            {'label': 'Italy', 'value': 'Italy'},
+            {'label': 'Sweden', 'value': 'Sweden'},
+            {'label': 'Switzerland', 'value': 'Switzerland'},
+            {'label': 'Australia', 'value': 'Australia'},
+            {'label': 'Austria', 'value': 'Austria'},
+            {'label': 'France', 'value': 'France'},
+            {'label': 'Germany', 'value': 'Germany'},
+            {'label': 'Turkey', 'value': 'Turkey'},
+            {'label': 'United States of America', 'value': 'US'},
+            {'label': 'New Zealand', 'value': 'New Zealand'},
+            {'label': 'United Kingdom', 'value': 'UK'},
+            {'label': 'Mexico', 'value': 'Mexico'}]
+
+
+stateOPTS = [
+    {'label':'Arkansas','value':"AR"},
+    {'label':'Alabama','value':"AL"},
+    {'label':'Alaska','value':"AK"},
+
+    {'label':'Arizona','value':"AZ"},
+    {'label':'Connecticut','value':'CT'},
+    {'label':'California','value':'CA'},
+    {'label':'Colorado','value':"CO"},
+    {'label':'Delaware','value':"DE"},
+
+    {'label':'Florida','value':"FL"},
+    {'label':'Georgia','value':"GA"},
+    {'label':'Hawaii','value':'HI'},
+    {'label':'Idaho','value':"ID"},
+
+    {'label':'Illinois','value':"IL"},
+    {'label':'Indiana','value':'IN'},
+    {'label':'Iowa','value':"IA"},
+    {'label':'Kansas','value':"KS"},
+
+    {'label':"Kentucky",'value':"KY"},
+    {'label':'Louisianna','value':"LA"},
+    {'label':"Massachusetts",'value':"MA"},
+    {'label':'Maine','value':"ME"},
+    {'label':'Maryland','value':"MD"},
+
+    {'label':'Michigan','value':"MI"},
+
+    {'label':'Minnesota','value':"MN"},
+    {'label':'Mississippi','value':'MS'},
+    {'label':'Missouri','value':'MO'},
+    {'label':'Montana','value':"MT"},
+    {'label':'North Dakota','value':"ND"},
+    {'label':'Nebraska','value':"NE"},
+
+    {'label':'Nevada','value':"NV"},
+    {'label':'New Hampshire','value':"NH"},
+    
+
+    {'label':'New Jersey','value':'NJ'},
+    {'label':'New Mexico','value':"NM"},
+
+    {'label':'New York','value':"NY"},
+    {'label':'North Carolina','value':'NC'},
+    {'label':'Ohio','value':"OH"},
+    {'label':'Oklahoma','value':"OK"},
+    {'label':'Oregon','value':'OR'},
+    {'label':'Pennsylvania','value':'PA'},
+    {'label':'Rhode Island','value':"RI"},
+    {'label':'South Carolina','value':'SC'},
+    {'label':'South Dakota','value':"SD"},
+    {'label':'Tennessee','value':"TN"},
+    {'label':'Texas','value':'TX'},
+    {'label':'Vermont','value':"VT"},
+
+    {'label':'Virginia','value':"VA"},
+    {'label':'Washington','value':'WA'},
+    {'label':'West Virginia','value':"WV"},
+
+    {'label':'Wisconsin','value':'WI'},
+    {'label':'Wyoming','value':'WY'}  
+    ]
+
+
+
+
+projOPTS = [
+            {'label': 'Con Edison', 'value': 'ConEd'},
+            {'label': 'Duke Ohio', 'value': 'DukeOH'},
+            {'label': 'Dom Questar (UT)', 'value': 'DomQuest'},
+            {'label': 'WEC Energy (WI)', 'value': 'WEC_WI'},
+            {'label': 'WPS MMD (WI)', 'value': 'WPS_WI'},
+            {'label': 'Peoples (IL)', 'value': 'PeoplesIL'},
+            {'label': 'ACLARA (NY)))', 'value': 'Aclara'},
+            {'label': 'Duke IPI', 'value': 'DukeIPI'},
+            {'label': 'Norwhich Public Utilities', 'value': 'norwhich'},
+            {'label': 'Trussville (AL)', 'value': 'Trussville'},
+            {'label': 'CPS (TX)))', 'value': 'CPS_TX'},
+            {'label': 'Dominion (SC)', 'value': 'DominionSC'},
+            {'label': 'Dominion (NC)', 'value': 'DominionNC'}
+        ]
+
+
+
+
+whichAvgOPTS = [
+        {'label': '7 Day ', 'value': 'sevenday'},
+        {'label': '3 Day', 'value': 'threeday'},
+        {'label': 'Daily', 'value': 'daily'},
+        {'label': 'Cumulative','value':'total'}
+    ]
+
+popOPTS = [
+    {'label':'Relative to Population','value':'relpop'},
+    {'label':'Raw Cases', 'value':'nonrelpop'}
+    
+    
+    ]
+
+
 server = app.server
 
 app.layout = html.Div([
