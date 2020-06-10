250496e3912cac3de2e0a34dfcaa1ca55d3d7172
elimywilliams <62444225+elimywilliams@users.noreply.github.com>
2020-06-10 11:53:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from plotly.subplots import make_subplots
 import plotly.express as px
 #import base64
 #import json
-import bs4 as bs
+#import bs4 as bs
 #from textwrap import dedent as d
 
 px.set_mapbox_access_token('pk.eyJ1IjoiZXdpbGxpYW1zMjAyMCIsImEiOiJja2FpdTIxOXMwM2wzMnFtbmVmb3IzZDJ6In0.TVsQ-iu8bN4PQLkBCr6tQQ')
