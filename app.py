25689409be256f62bdda49f2e7e4a7623bd9b23c
elimywilliams <62444225+elimywilliams@users.noreply.github.com>
2020-06-10 11:39:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -27,58 +27,6 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css',
                         'https://raw.githubusercontent.com/elimywilliams/sc_covid19/master/header2.css'  ,
                         'https://github.com/plotly/dash-app-stylesheets/blob/master/dash-oil-and-gas.css'
                         ]
-def retorna_documento(numero):
-    if (numero == "Spain"):
-        name = 'https://raw.githubusercontent.com/elimywilliams/sc_covid19/master/spainInfo.txt'
-    elif (numero == "Italy"):
-        name = 'https://raw.githubusercontent.com/elimywilliams/sc_covid19/master/italyInfo.txt'
-    elif (numero == "United Kingdom"):
-        name = 'https://raw.githubusercontent.com/elimywilliams/sc_covid19/master/ukInfo.txt'
-    elif (numero == 'France'):
-        name = 'https://raw.githubusercontent.com/elimywilliams/sc_covid19/master/franceInfo.txt'
-    elif (numero == 'Sweden'):
-        name = 'https://raw.githubusercontent.com/elimywilliams/sc_covid19/master/swedenInfo.txt'
-    elif (numero == 'Switzerland'):
-        name = 'https://raw.githubusercontent.com/elimywilliams/sc_covid19/master/switzerlandInfo.txt'
-    elif (numero == 'Australia'):
-        name = 'https://raw.githubusercontent.com/elimywilliams/sc_covid19/master/aussieInfo.txt'
-    elif (numero == 'Austria'):
-        name = 'https://raw.githubusercontent.com/elimywilliams/sc_covid19/master/austriaInfo.txt'
-    elif (numero == 'Germany'):
-        name = 'https://raw.githubusercontent.com/elimywilliams/sc_covid19/master/germanyInfo.txt'
-    elif (numero == 'Turkey'):
-        name = 'https://raw.githubusercontent.com/elimywilliams/sc_covid19/master/turkeyInfo.txt'
-    elif (numero == 'New Zealand'):
-        name = 'https://raw.githubusercontent.com/elimywilliams/sc_covid19/master/nzInfo.txt'
-    elif (numero == 'US'):
-        name = 'https://raw.githubusercontent.com/elimywilliams/sc_covid19/master/usInfo.txt'
-    page = requests.get(name)
-    html_doc = page.text.replace('/n','')
-    #with open(name, 'r') as file:
-    #    html_doc = file.read().replace('\n', '')    
-    return convert_html_to_dash(html_doc)
-
-def convert_html_to_dash(el,style = None):
-    CST_PERMITIDOS =  {'div','span','a','hr','br','p','b','i','u','s','h1','h2','h3','h4','h5','h6','ol','ul','li',
-                        'em','strong','cite','tt','pre','small','big','center','blockquote','address','font','img',
-                        'table','tr','td','caption','th','textarea','option'}
-    def __extract_style(el):
-        if not el.attrs.get("style"):
-            return None
-        return {k.strip():v.strip() for k,v in [x.split(": ") for x in el.attrs["style"].split(";")]}
-
-    if type(el) is str:
-        return convert_html_to_dash(bs.BeautifulSoup(el,'html.parser'))
-    if type(el) == bs.element.NavigableString:
-        return str(el)
-    else:
-        name = el.name
-        style = __extract_style(el) if style is None else style
-        contents = [convert_html_to_dash(x) for x in el.contents]
-        if name.title().lower() not in CST_PERMITIDOS:        
-            return contents[0] if len(contents)==1 else html.Div(contents)
-        return getattr(html,name.title())(contents,style = style)
-
 
 
 file_name = 'https://raw.githubusercontent.com/elimywilliams/sc_covid19/master/usoverall.csv'
