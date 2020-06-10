970169552b65512585213e0d4c51de002031ff7c
elimywilliams <62444225+elimywilliams@users.noreply.github.com>
2020-06-10 11:50:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ import plotly.express as px
 #import base64
 #import json
 import bs4 as bs
-import requests
 #from textwrap import dedent as d
 
 px.set_mapbox_access_token('pk.eyJ1IjoiZXdpbGxpYW1zMjAyMCIsImEiOiJja2FpdTIxOXMwM2wzMnFtbmVmb3IzZDJ6In0.TVsQ-iu8bN4PQLkBCr6tQQ')
